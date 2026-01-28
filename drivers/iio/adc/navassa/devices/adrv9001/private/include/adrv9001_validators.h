analogdevicesinc/linux

iio: adc: add adrv9002 support
The ADRV9002 is a highly integrated, RF transceiver offering dual channel
transmitters and dual channel receivers, integrated synthesizers, and
digital signal processing functions. The IC delivers a versatile
combination of high performance and low power consumption required by
battery powered radio equipment and can operate in both FDD and TDD modes.
The ADRV9002 operates from 30 MHz to 6000 MHz covering the VHF, licensed
and unlicensed cellular bands, and ISM bands. The IC is capable of
supporting both narrowband and wideband standards up to 40MHz bandwidth on
both receive and transmit.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>

drivers/iio/adc/navassa/devices/adrv9001/private/include/adrv9001_validators.h
@@ -1,0 +1,78 @@
+/**
+ * \file
+ * \brief Contains ADRV9001 parameters validation related private function prototypes
+ *
+ * ADRV9001 API Version: $ADI_ADRV9001_API_VERSION$
+ */
+
+ /**
+ * Copyright 2019 Analog Devices Inc.
+ * Released under the ADRV9001 API license, for more information
+ * see the "LICENSE.txt" file in this zip file.
+ */
+
+#ifndef _ADRV9001_VALIDATORS_H_
+#define _ADRV9001_VALIDATORS_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*
+*********************************************************************************************************
+*                                             INCLUDE FILES
+*********************************************************************************************************
+*/
+
+/* Header files related to libraries */
+#include "adi_adrv9001_user.h"
+
+/* System header files */
+
+/*********************************************************************************************************/
+/**
+ * \brief Check for valid channels
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] channels      Set of port/channel combinations to prime
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Channel_Validate(adi_adrv9001_Device_t *adrv9001,
+                                      adi_common_ChannelNumber_e channel);
+/**
+ * \brief Check for valid ports
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] ports         Set of port/channel combinations to prime
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Port_Validate(adi_adrv9001_Device_t *adrv9001,
+                                   adi_common_Port_e port);
+
+/**
+ * \brief Check the following:
+ *        1. whether ARM binary is loaded
+ *        2. ports and channels are valid
+ *
+ * \brief Channel state is one of [STANDBY, CALIBRATED, PRIMED, RF ENABLED]
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] ports         Set of port/channel combinations to prime
+ * \param[in] channels      Set of port/channel combinations to prime
+ * \param[in] length        Length of ports and channels arrays - the number of port/channel combinations to prime; 
+ *                          Each index i of ports and channels correspond to a single port/channel combination
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Channel_State_GenericValidate(adi_adrv9001_Device_t *adrv9001,
+                                                   adi_common_Port_e ports[],
+                                                   adi_common_ChannelNumber_e channels[],
+                                                   uint8_t length);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ADRV9001_VALIDATORS_H_ */

m1nl/analogdevicesinc-linux

drivers/iio/adc/navassa/devices/adrv9001/private/include/adrv9001_validators.h
@@ -1,0 +1,78 @@
+/**
+ * \file
+ * \brief Contains ADRV9001 parameters validation related private function prototypes
+ *
+ * ADRV9001 API Version: $ADI_ADRV9001_API_VERSION$
+ */
+
+ /**
+ * Copyright 2019 Analog Devices Inc.
+ * Released under the ADRV9001 API license, for more information
+ * see the "LICENSE.txt" file in this zip file.
+ */
+
+#ifndef _ADRV9001_VALIDATORS_H_
+#define _ADRV9001_VALIDATORS_H_
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+/*
+*********************************************************************************************************
+*                                             INCLUDE FILES
+*********************************************************************************************************
+*/
+
+/* Header files related to libraries */
+#include "adi_adrv9001_types.h"
+
+/* System header files */
+
+/*********************************************************************************************************/
+/**
+ * \brief Check for valid channels
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] channels      Set of port/channel combinations to prime
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Channel_Validate(adi_adrv9001_Device_t *adrv9001,
+                                      adi_common_ChannelNumber_e channel);
+/**
+ * \brief Check for valid ports
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] ports         Set of port/channel combinations to prime
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Port_Validate(adi_adrv9001_Device_t *adrv9001,
+                                   adi_common_Port_e port);
+
+/**
+ * \brief Check the following:
+ *        1. whether ARM binary is loaded
+ *        2. ports and channels are valid
+ *
+ * \brief Channel state is one of [STANDBY, CALIBRATED, PRIMED, RF ENABLED]
+ *
+ * \param[in] adrv9001      Context variable - Pointer to the ADRV9001 device data structure containing settings
+ * \param[in] ports         Set of port/channel combinations to prime
+ * \param[in] channels      Set of port/channel combinations to prime
+ * \param[in] length        Length of ports and channels arrays - the number of port/channel combinations to prime; 
+ *                          Each index i of ports and channels correspond to a single port/channel combination
+ *
+ * \returns A code indicating success (ADI_COMMON_ACT_NO_ACTION) or the required action to recover
+ */
+int32_t adi_adrv9001_Channel_State_GenericValidate(adi_adrv9001_Device_t *adrv9001,
+                                                   adi_common_Port_e ports[],
+                                                   adi_common_ChannelNumber_e channels[],
+                                                   uint8_t length);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ADRV9001_VALIDATORS_H_ */